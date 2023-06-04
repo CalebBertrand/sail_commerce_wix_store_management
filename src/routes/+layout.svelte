d766c764c8053cde578dbf807c23cb0dcd0e3279
Caleb <caleb.bertrand@verisglobal.com>
2023-06-04 00:21:32 -0500

    Starting on product mngmnt main page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<script>
+	import {
+		Header,
+		Content,
+		Theme,
+		HeaderUtilities,
+		HeaderGlobalAction,
+	} from "carbon-components-svelte";
+	import { Moon, Sun } from "carbon-icons-svelte";
+
+
+	const darkMode = 'g90';
+	const lightMode = 'white';
+	let theme = darkMode; // default theme
+</script>
+
+<Theme persist persistKey="sail-commerce-theme" bind:theme />
+
+<Header company="Sail Commerce" platformName="Product Management">
+	<HeaderUtilities>
+		<HeaderGlobalAction
+			aria-label="Color Theme"
+			icon={theme == darkMode ? Moon : Sun}
+			on:click={() => {
+				theme = theme == darkMode ? lightMode : darkMode;
+			}}
+		/>
+	</HeaderUtilities>
+</Header>
+
+<Content>
+	<slot />
+</Content>
+
+<style>
+	@import "../app.css";
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,53 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
+	import { ClickableTile, Button, Search } from "carbon-components-svelte";
+	import { onMount } from "svelte";
+	import { fade } from "svelte/transition";
+	import { TrashCan } from "carbon-icons-svelte";
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+
+	let searchValue = "";
+	let listShown = false;
+
+	onMount(() => {
+		listShown = true;
+	});
+
+	$: filteredProducts = data.products.filter(
+		(product) =>
+			!searchValue ||
+			product.name.toLowerCase().includes(searchValue.toLowerCase())
+	);
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<ul>
-    {#each data.products as product}
-        <li>{product.name}</li>
-    {/each}
-</ul>
+<div class="mb-6">
+	<h1 class="float-left">Products</h1>
+
+	<div class="float-right w-full md:w-1/3">
+		<Search bind:value={searchValue} />
+	</div>
+
+	<div class="clear-both" />
+</div>
+
+<!-- To trigger the transition as these are prerendered -->
+{#if listShown}
+	{#each filteredProducts as product, index}
+		<div transition:fade={{ delay: index * 75, duration: 200 }}>
+			<ClickableTile class="mb-3">
+				<h4 class="float-left">{product.name}</h4>
+				<div class="float-right">
+					<Button class="uppercase tracking-widest" kind="primary">Edit</Button>
+					<Button
+						kind="danger-tertiary"
+						iconDescription="Delete"
+						icon={TrashCan}
+					/>
+				</div>
+
+				<div class="clear-both" />
+			</ClickableTile>
+		</div>
+	{/each}
+{/if}
